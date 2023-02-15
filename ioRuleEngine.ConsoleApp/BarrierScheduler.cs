6476151b9886990246af285ee4839873dc94d65e
Iulian Caluian <iulian.coder@gmail.com>
2023-02-15 22:44:23 +0200

    Writing a prototype of the rule engine

---
+++ ioRuleEngine.ConsoleApp/BarrierScheduler.cs
@@ -0,0 +1,61 @@
+﻿using Quartz.Impl;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ioRuleEngine.ConsoleApp
+{
+    /// <summary>
+    /*
+     * // Example usage
+        var scheduler = new BarrierScheduler();
+        await scheduler.Start();
+
+        // Wait for the scheduler to run for some time
+        await Task.Delay(TimeSpan.FromHours(10));
+
+        await scheduler.Stop();
+     */
+    /// </summary>
+    public class BarrierScheduler
+    {
+        private IScheduler _scheduler;
+
+        public async Task Start()
+        {
+            // Create a new scheduler
+            var factory = new StdSchedulerFactory();
+            _scheduler = await factory.GetScheduler();
+
+            // Define the job to execute
+            var job = JobBuilder.Create<BarrierJob>()
+                .WithIdentity("BarrierJob", "BarrierGroup")
+                .Build();
+
+            // Define the trigger to run the job at 9am and 6pm every day
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("BarrierTrigger", "BarrierGroup")
+                .WithDailyTimeIntervalSchedule(x => x
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(9, 0))
+                    .EndingDailyAt(TimeOfDay.HourAndMinuteOfDay(18, 0))
+                )
+                .Build();
+
+            // Schedule the job with the trigger
+            await _scheduler.ScheduleJob(job, trigger);
+
+            // Start the scheduler
+            await _scheduler.Start();
+        }
+
+        public async Task Stop()
+        {
+            // Shut down the scheduler
+            await _scheduler.Shutdown();
+        }
+    }
+}
--- ioRuleEngine.ConsoleApp/Program.cs
+++ ioRuleEngine.ConsoleApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using ioRuleEngine.ConsoleApp;
+using Newtonsoft.Json;
+using Quartz;
+using Quartz.Impl;
+
+
+// Load the rules from a JSON file
+string rulesFilePath = "rules.json";
+
+using (var streamReader = new StreamReader(rulesFilePath))
+using (var jsonReader = new JsonTextReader(streamReader))
+{
+    var serializer = new JsonSerializer();
+    var rules = serializer.Deserialize<List<BarrierRule>>(jsonReader);
+
+    var scheduler = StdSchedulerFactory.GetDefaultScheduler().Result;
+    scheduler.Start();
+
+    var ruleEngine = new BarrierRuleEngine(rules, scheduler);
+    ruleEngine.StartAsync();
+}
+
+
+
